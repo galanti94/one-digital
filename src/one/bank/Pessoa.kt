fb29764a2acb7816ec1c720acd399edab5d1886e
galanti94 <galanti_imp@hotmail.com>
2021-07-22 01:14:53 -0300

    modo private no set

--- src/one/bank/Pessoa.kt
+++ src/one/bank/Pessoa.kt
@@ -1,8 +1,21 @@
 package one.bank
 
+// Outerclass, classe de fora
 class Pessoa {
+
     var nome: String = "Guilherme"
     var cpf: String = "123.456.789-10"
+
+    private set
+/*
+    // Não dá para usar sempre o inner class
+    // Usado somente em casos especiais, casos de parses
+    inner class Endereco{
+        var rua: String = "Rua teste"
+        var numero: Int = 10
+    }
+
+ */
 }
 
 fun main(){
@@ -10,4 +23,6 @@ fun main(){
 
     println(guilherme.nome)
     println(guilherme.cpf)
+
+    //println(guilherme.Endereco().rua)
 }
