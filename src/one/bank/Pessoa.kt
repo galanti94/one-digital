afd8a37e3a6e4a3489de57b9f3f62fabfe360514
galanti94 <galanti_imp@hotmail.com>
2021-07-23 00:43:01 -0300

    created open class e class Funcionario

--- src/one/bank/Pessoa.kt
+++ src/one/bank/Pessoa.kt
@@ -1,32 +1,7 @@
 package one.bank
 
-// Outerclass, classe de fora
-class Pessoa {
-
-    var nome: String = "Guilherme"
-    var cpf: String = "123.456.789-10"
-
-    private set // Não deixa alterar fora do escopo da classe
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-/*
-    // Não dá para usar sempre o inner class
-    // Usado somente em casos especiais, casos de parses
-    inner class Endereco{
-        var rua: String = "Rua teste"
-        var numero: Int = 10
-    }
-
- */
-}
-
-fun main(){
-    val guilherme = Pessoa()
-
-    println(guilherme.pessoaInfo())
-
-
-    //println(guilherme.Endereco().rua)
-}
+// open permite a herança
+open class Pessoa (
+    open val nome: String,
+    open val cpf: String,
+)
---
+++ src/one/bank/testes/TestePessoa.kt
@@ -0,0 +1,11 @@
+package one.bank.testes
+
+import one.bank.Pessoa
+
+fun main(){
+    val guilherme = Pessoa(nome = "Guilherme", cpf = "124232")
+
+    println(guilherme.nome)
+    println(guilherme.cpf)
+
+}
